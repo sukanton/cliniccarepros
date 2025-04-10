f2681bd0a21cc79f59ef763a90b58cb7b14b2098
sukanton <sukan.ton@gmail.com>
2025-04-10 11:25:34 +0700

    Fix template path for deployment

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import os
 
 app = Flask(__name__, template_folder="cliniccarepros/templates")
 
-
 # ---------- PATH FOR DATABASE ----------
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, "clinic.db")
